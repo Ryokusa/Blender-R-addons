43a42281b6ceed89159010d2ea1ab320dbdface3
Ryokusasa <mattya.mqdm@gmail.com>
2024-03-27 19:12:11 +0900

    fix: CM3D2関連削除

--- BR Addon/misc_VIEW3D_MT_edit_mesh_split.py
+++ BR Addon/misc_VIEW3D_MT_edit_mesh_split.py
@@ -3,30 +3,32 @@ import bmesh
 from . import common
 from . import compat
 
+
 # メニュー等に項目追加
 def menu_func(self, context):
     icon_id = common.kiss_icon()
     self.layout.separator()
-    self.layout.label(text="CM3D2", icon_value=icon_id)
-    self.layout.operator('mesh.split_sharp')
+    self.layout.label(text="BR", icon_value=icon_id)
+    self.layout.operator("mesh.split_sharp")
+
 
 @compat.BlRegister()
 class CNV_OT_split_sharp(bpy.types.Operator):
-    bl_idname = 'mesh.split_sharp'
+    bl_idname = "mesh.split_sharp"
     bl_label = "Split Sharp Edges"
     bl_description = "Split all edges marked as sharp"
-    bl_options = {'REGISTER', 'UNDO'}
+    bl_options = {"REGISTER", "UNDO"}
 
     @classmethod
     def poll(cls, context):
         ob = context.active_object
-        return ob and ob.type == 'MESH' and ob.mode == 'EDIT'
+        return ob and ob.type == "MESH" and ob.mode == "EDIT"
 
     def execute(self, context):
         ob = context.active_object
         me = ob.data
 
-        bpy.ops.mesh.select_all(action='DESELECT')
+        bpy.ops.mesh.select_all(action="DESELECT")
 
         bm = bmesh.from_edit_mesh(me)
 
@@ -36,7 +38,7 @@ class CNV_OT_split_sharp(bpy.types.Operator):
                 sharp_edges.append(edge)
 
         changed = bmesh.ops.split_edges(bm, edges=sharp_edges)
-        for edge in changed['edges']:
+        for edge in changed["edges"]:
             edge.select_set(True)
-        
-        return {'FINISHED'}
+
+        return {"FINISHED"}
